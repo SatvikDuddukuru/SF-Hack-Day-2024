50f1938684f40f8b63a539a26b02087d6af77e83
Amaan Chaudhry <amaanchau7@gmail.com>
2024-06-11 18:23:51 -0500

    Fixed Routing

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,18 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../navbar.css'; // Ensure this path is correct
+import { Link } from 'react-router-dom'; 
+import '../navbar.css';
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="custom-navbar">
       <Container>
-        <Navbar.Brand href="#home" className="text-white">AccesiSlide</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="text-white">
+          AccesiSlide
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className="text-white">Home</Nav.Link>
-            <Nav.Link href="#link" className="text-white">About</Nav.Link>
+            <Nav.Link as={Link} to="/" className="text-white">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" className="text-white">About</Nav.Link>
             <Nav.Link href="#link" className="text-white">Meet the Creators</Nav.Link>
           </Nav>
         </Navbar.Collapse>
